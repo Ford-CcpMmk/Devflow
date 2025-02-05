019868ce071d6b558e62dbc7ece74933e47a5305
Ford-CcpMmk <Fordccp.mmk@gmail.com>
2025-02-05 17:44:53 +0700

    Tailwind CSS Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 const Home = () => (
-  <h1 className="text-3xl font-bold text-violet-700 ">
+  <h1 className="text-3xl font-bold text-primary-500">
     Welcome to the world of Next.js
   </h1>
 );
