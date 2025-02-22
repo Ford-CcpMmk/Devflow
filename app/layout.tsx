aee4fb4a36c6df92e0f8e96b438966e1ef824f7a
Ford-CcpMmk <Fordccp.mmk@gmail.com>
2025-02-22 18:47:28 +0700

    Implement Github SignIn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from "next";
-import React from "react";
+import { ReactNode } from "react";
 import { Inter, Space_Grotesk as SpaceGrotesk } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
-import Navbar from "@/components/navigation/navbar";
+import { Toaster } from "sonner";
+import { SessionProvider } from "next-auth/react";
+import { auth } from "@/auth";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -26,28 +28,31 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+const RootLayout = async ({ children }: { children: ReactNode }) => {
+  // It retrieves the current session from cookies stored in the browser.
+  const session = await auth();
+
   return (
     <html lang="en" suppressHydrationWarning>
-      {/* if you use .className, it will set it as defualt font of the application. */}
-      <body
-        className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
-      >
-        {/* Basically, the HTML created on the server is sent to the browser. But as soon as it's loaded in the browser, the next theme updates elements, rusulting in a different HTML skeleton. This is what a hydration mismatch is. To fix this issue, which is really not an issue, NextTheme recommends suppressing hydration warnings. It will only suppress hydration warnings at one level deep. Only this one coming from Next Theme.*/}
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
+      <SessionProvider session={session}>
+        {/* if you use .className, it will set it as defualt font of the application. */}
+        <body
+          className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
         >
-          <Navbar />
-          {children}
-        </ThemeProvider>
-      </body>
+          {/* Basically, the HTML created on the server is sent to the browser. But as soon as it's loaded in the browser, the next theme updates elements, rusulting in a different HTML skeleton. This is what a hydration mismatch is. To fix this issue, which is really not an issue, NextTheme recommends suppressing hydration warnings. It will only suppress hydration warnings at one level deep. Only this one coming from Next Theme.*/}
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+          <Toaster position="top-center" richColors />
+        </body>
+      </SessionProvider>
     </html>
   );
-}
+};
+
+export default RootLayout;
