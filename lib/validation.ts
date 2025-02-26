240848bf31a9c779eb9b027444db3422f4fd8045
Ford-CcpMmk <Fordccp.mmk@gmail.com>
2025-02-26 23:35:55 +0700

    Implement the UI of the Question Form

--- lib/validation.ts
+++ lib/validation.ts
@@ -48,3 +48,22 @@ export const SignUpSchema = z.object({
       message: "Password must contain at least one special character.",
     }),
 });
+
+export const AskQuestionSchema = z.object({
+  title: z
+    .string()
+    .min(5, { message: "Title must be at least 10 characters long." })
+    .max(100, { message: "Title cannot exceed 100 characters." }),
+
+  content: z.string().min(1, { message: "Body is required" }),
+
+  tags: z
+    .array(
+      z
+        .string()
+        .min(1, { message: "Tag is required." })
+        .max(30, { message: "Tag cannot exceed 30 characters." })
+    )
+    .min(1, { message: "Please select at least one tag." })
+    .max(3, { message: "Cannot add more than 3 tags" }),
+});
